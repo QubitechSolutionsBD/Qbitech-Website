334f81a03a39bbb4cfde2900bba23c2049ef872c
ruhan-quick <ruhanquick@gmail.com>
2021-06-09 00:29:20 +0600

    contact page responsive

--- client/src/components/Contact/ContactLeft.js
+++ client/src/components/Contact/ContactLeft.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+
 //icons
 import location from "../../assets/contact/location.png";
 import mail from "../../assets/contact/mail.png";
@@ -9,36 +11,43 @@ import linkedin from "../../assets/social-links/LinkedIn.png";
 const ContactLeft = () => {
   return (
     <>
-      <h3>Get In Touch</h3>
-      <ul>
-        <li>
-          {" "}
-          <img src={location} alt="" /> &nbsp;&nbsp;31 Talaimari,Kazla ,Rajshahi
-        </li>
-        <li>
-          <img src={phone} alt="" />
-          &nbsp;&nbsp;+8801912117126
-        </li>
-        <li>
-          <img src={mail} alt="" />
-          &nbsp;&nbsp;qubitechsoltions@gmail.com
-        </li>
-      </ul>
-      <h3>Get In Touch</h3>
-      <ul>
-        <li>
-          {" "}
-          <img src={facebook} alt="" /> &nbsp;&nbsp;Qubitech Solutions
-        </li>
-        <li>
-          <img src={instagram} alt="" />
-          &nbsp;&nbsp;QubitechSlt
-        </li>
-        <li>
-          <img src={linkedin} alt="" />
-          &nbsp;&nbsp;Qubitech Solutions
-        </li>
-      </ul>
+      <Grid container>
+        <Grid item xs={12} sm={6} lg={12}>
+          <h3>Get In Touch</h3>
+          <ul>
+            <li>
+              {" "}
+              <img src={location} alt="" /> &nbsp;&nbsp;31 Talaimari,Kazla
+              ,Rajshahi
+            </li>
+            <li>
+              <img src={phone} alt="" />
+              &nbsp;&nbsp;+8801912117126
+            </li>
+            <li>
+              <img src={mail} alt="" />
+              &nbsp;&nbsp;qubitechsoltions@gmail.com
+            </li>
+          </ul>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={12}>
+          <h3>Get In Touch</h3>
+          <ul>
+            <li>
+              {" "}
+              <img src={facebook} alt="" /> &nbsp;&nbsp;Qubitech Solutions
+            </li>
+            <li>
+              <img src={instagram} alt="" />
+              &nbsp;&nbsp;QubitechSlt
+            </li>
+            <li>
+              <img src={linkedin} alt="" />
+              &nbsp;&nbsp;Qubitech Solutions
+            </li>
+          </ul>
+        </Grid>
+      </Grid>
     </>
   );
 };
