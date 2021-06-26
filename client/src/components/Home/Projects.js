203bb61bc1c302d6d7efe4137ccc06c75ee4d580
MizanurRahmann <mr926560@gmail.com>
2021-06-26 14:02:03 +0600

    :art: UPDATE: landing page case studies section.

--- client/src/components/Home/Projects.js
+++ client/src/components/Home/Projects.js
@@ -1,41 +1,99 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
+import { Link } from "react-router-dom";
+
+// ASSETS
+import right from "../../assets/pageservice/down-arrow.png";
+import left from "../../assets/pageservice/up-arrow.png";
 
 function Projects() {
+  const customSlider = useRef();
+
   const settings = {
-    dots: true,
+    dots: false,
+    arrows: false,
     infinite: true,
-    speed: 500,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    arrows: true,
-    dotsClass: "slick-dots slick-thumb",
-    customPaging: (i) => {
-      return (
-        <div className="custom-dots">
-          <div className="box"></div>
-        </div>
-      );
-    },
+    initialSlide: 0,
+    speed: 700,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div className="home__projects">
-      <div className="projects">
-        <div className="heading">
-          <h2 id="homeprojectsheading">Case Studies</h2>
-        </div>
-        <Slider {...settings} className="slider">
-          <div className="project">
-            <div className="container" id="homeprojectcard"></div>
+      <div className="head">
+        <h1>
+          <div>
+            <span>Case</span>
           </div>
-          <div className="project">
-            <div className="container" id="homeprojectcard"></div>
+          <div>
+            <span>Studies</span>
           </div>
-          <div className="project">
-            <div className="container" id="homeprojectcard"></div>
+        </h1>
+        <p>
+          Explore our recent selected projects and witness our full 360
+          transformations come to life.
+        </p>
+        <Link to="/">Explore More +</Link>
+      </div>
+
+      <div className="content">
+        <Slider
+          {...settings}
+          className="slider"
+          ref={(slider) => (customSlider.current = slider)}
+        >
+          <div className="work">
+            <div className="container">1</div>
+          </div>
+          <div className="work">
+            <div className="container">2</div>
+          </div>
+          <div className="work">
+            <div className="container">3</div>
+          </div>
+          <div className="work">
+            <div className="container">4</div>
+          </div>
+          <div className="work">
+            <div className="container">5</div>
           </div>
         </Slider>
+        <div className="slider-btn-box">
+          <button onClick={() => customSlider.current.slickPrev()}>
+            <img src={right} alt="" />
+          </button>
+          <button onClick={() => customSlider.current.slickNext()}>
+            <img src={left} alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
