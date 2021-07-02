1e953989773ad21d715a39c29adc4fa1e57cffac
ruhan-quick <ruhanquick@gmail.com>
2021-07-03 01:04:36 +0600

    animation added to 4 section

--- client/src/components/Services/WeDev2.js
+++ client/src/components/Services/WeDev2.js
@@ -4,45 +4,46 @@ const WeDev2 = () => {
   return (
     <div className="services__wedev2">
       <div className="circle" id="globalBannerCircle1"></div>
-
-      <h1>
-        <div>
-          <span id="serviceswedev2head">
-            We develop high-end marketing websites and stable
-          </span>
-        </div>
-        <div>
-          <span id="serviceswedev2head">
-            architecture for mobile and desktop applications for
-          </span>
-        </div>
-        <div>
-          <span id="serviceswedev2head">our clients.</span>
-        </div>
-      </h1>
-      <h2>
-        We develop high-end marketing websites and stable architecture for
-        mobile and desktop applications for ur clients.
-      </h2>
-      <p>
-        <div>
-          <span>
-            From custom front-end to sophisticated back-end, our in-house
-            development
-          </span>
-        </div>
-        <div>
-          <span>
-            team handles the heavy lifting to create an engaging experience
-            online.
-          </span>
-        </div>
-      </p>
-      <h3>
-        From custom front-end to sophisticated back-end, our in-house
-        development team handles the heavy lifting to create an engaging
-        experience online.
-      </h3>
+      <div className="heading-container">
+        <h1>
+          <div>
+            <span id="serviceswedev2head">
+              We develop high-end marketing websites and stable
+            </span>
+          </div>
+          <div>
+            <span id="serviceswedev2head">
+              architecture for mobile and desktop applications for
+            </span>
+          </div>
+          <div>
+            <span id="serviceswedev2head">our clients.</span>
+          </div>
+        </h1>
+        <h2>
+          We develop high-end marketing websites and stable architecture for
+          mobile and desktop applications for ur clients.
+        </h2>
+        <p>
+          <div>
+            <span>
+              From custom front-end to sophisticated back-end, our in-house
+              development
+            </span>
+          </div>
+          <div>
+            <span>
+              team handles the heavy lifting to create an engaging experience
+              online.
+            </span>
+          </div>
+        </p>
+        <h3>
+          From custom front-end to sophisticated back-end, our in-house
+          development team handles the heavy lifting to create an engaging
+          experience online.
+        </h3>
+      </div>
     </div>
   );
 };
